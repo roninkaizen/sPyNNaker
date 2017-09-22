eb42ad7ef878a66ea527f9fd1db2e7d896427070
oliverrhodes <oliver.rhodes@manchester.ac.uk>
2017-09-22 15:22:21 +0100

    Create custom STDP rule based on spike pairs and additive weights

--- spynnaker/pyNN/models/neuron/plasticity/stdp/timing_dependence/__init__.py
+++ spynnaker/pyNN/models/neuron/plasticity/stdp/timing_dependence/__init__.py
@@ -6,9 +6,9 @@ from .timing_dependence_recurrent import TimingDependenceRecurrent
 from .timing_dependence_spike_nearest_pair \
     import TimingDependenceSpikeNearestPair
 from .timing_dependence_vogels_2011 import TimingDependenceVogels2011
+from .timing_dependence_spike_pair_custom import TimingDependenceSpikePairCustom
 
-__all__ = [
-    "AbstractTimingDependence", "TimingDependenceSpikePair",
-    "TimingDependencePfisterSpikeTriplet", "TimingDependenceRecurrent",
-    "TimingDependenceSpikeNearestPair", "TimingDependenceVogels2011"
-]
+__all__ = ["AbstractTimingDependence", "TimingDependenceSpikePair",
+        "TimingDependencePfisterSpikeTriplet", "TimingDependenceRecurrent",
+        "TimingDependenceSpikeNearestPair", "TimingDependenceVogels2011",
+        "TimingDependenceSpikePairCustom"]
